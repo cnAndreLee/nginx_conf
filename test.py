69c658c067e9710c5388e3c44599f5243eca315d
ningweiyu <ningweiyu@tptsct.com>
2021-06-09 19:45:28 +0800

    using docker-py to run containers

--- test.py
+++ test.py
@@ -2,6 +2,10 @@
 # -*- coding:utf-8 -*-
 # Author: Andre
 
+import docker
+import time
+import os
+
 class FMS:
     name=''
     mqtt_ip=''
@@ -74,9 +78,9 @@ def setactproxy():
         print("list is empty")
         exit()
     elif len(finnalActArray) == 1:
-        print("the car will be set is {}\n".format(finnalActArray[0]))
+        print("\n**************\n  the car will be set is {}\n**************".format(finnalActArray[0]))
     else:
-        print("the cars will be set are {}\n".format(finnalActArray))
+        print("\n**************\n  the cars will be set are {}\n**************".format(finnalActArray))
 
     portlist=[]
     for n in finnalActArray:
@@ -88,20 +92,67 @@ def setactproxy():
     while 1:
         fmsStr=input("Now, input the FMS which you want cars to connect , such as A or B:\n")
 
-        if fmsStr=='A' or fmsStr=='a':
-            fmsStr='A'
-            break
-        elif fmsStr=='B' or fmsStr=='b':
-            fmsStr='B'
+        #if fmsStr=='A' or fmsStr=='a':
+        #    fmsStr='A'
+        #    break
+        #elif fmsStr=='B' or fmsStr=='b':
+        #    fmsStr='B'
+        #    break
+        if fmsStr in FMS_DICT:
             break
         else:
             print("Input error !!!")
 
+    client = docker.from_env()
+    apiClient = docker.APIClient(base_url='unix://var/run/docker.sock')
+
+    # start
     for index in range(len(portlist)):
-        generate_nginx_conf(portlist[index], FMS_DICT[fmsStr].mqtt_ip, FMS_DICT[fmsStr].mqtt_port )
+        NginxConf=generate_nginx_conf(portlist[index], FMS_DICT[fmsStr].mqtt_ip, FMS_DICT[fmsStr].mqtt_port )
+
+        with open(TMP_FILENAME, 'w') as f:
+            f.write(NginxConf)
+        #f=open(TMP_FILENAME, 'w')
+        #f.write(NginxConf)
+        #f.close()
+
+        first_name="ACT_A{}_".format(str(portlist[index])[1:4])
+        name=first_name+fmsStr
+        if DEBUG:
+            print("first_name is "+first_name)
+
+        #cmd="docker stop {0}A {0}B ; docker rm {0}A {0}B".format(first_name)
+        #if DEBUG:
+        #    print("CMD is "+cmd)
+
+        #run cmd
+        for key in FMS_DICT:
+            print("deleting container < "+first_name+key+" >")
+            try:
+                apiClient.stop(first_name+key)
+                apiClient.remove_container(first_name+key)
+            except docker.errors.NotFound:
+                pass
 
 
-DEBUG=True
+        #cmd="docker run -d --name {0} --restart=always -p 0.0.0.0:{1}:{1}/tcp -v {2}:/etc/nginx/nginx.conf:ro nginx:latest".format(name, portlist[index], TMP_FILENAME)
+        #if DEBUG:
+        #    print("runCMD is "+cmd)
+
+        print(client.containers.run('nginx:latest', detach=True, name=name, restart_policy={"Name": "always"}, ports={str(portlist[index])+'/tcp':('0.0.0.0', portlist[index])}, volumes={TMP_FILENAME:{'bind':'/etc/nginx/nginx.conf', 'mode': 'ro'}}))
+
+        time.sleep(2)
+
+    #rm tmpfile
+    if DEBUG==False:
+        os.remove(TMP_FILENAME)
+
+    client.close()
+    apiClient.close()
+
+
+DEBUG=False
+TMP_FILENAME="/tmp/nginx_tmp.conf"
 
 print("-----------------------------------------")
 FMS_DICT={'A':FMS("FMS_A", "172.28.129.196", 30845, "172.28.138.55", 5000), 'B': FMS("FMS_B", "172.29.60.11",   1883,  "172.29.60.11",  6000)}
@@ -111,7 +162,13 @@ print("----------------------------------------\n")
 
 def main():
    
+    #client = docker.from_env()
+    #print(client.containers.list())
+    #print(type(client.containers.list()))
+    #print(type(client.containers.list()[0]))
+
     # selector=input("Please input the system which you want to operate, 'TOS', 'XJD', 'XCC' or 'ACT' ")
+
     setactproxy()
 
 
